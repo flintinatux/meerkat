044a6f37fcf529e9f2e03e1f9b75b58cf59d9141
Scott McCormack <scott.mccormack@articulate.com>
2016-08-07 23:30:13 -0400

    Convert example 2 to redux and add reset button

--- client/examples/02-text-fields.js
+++ client/examples/02-text-fields.js
@@ -1,35 +1,42 @@
 const assoc   = require('ramda/src/assoc')
 const compose = require('ramda/src/compose')
+const flip    = require('ramda/src/flip')
 const j2c     = require('j2c')
 const K       = require('ramda/src/always')
 const m       = require('mithril')
 const reverse = require('ramda/src/reverse')
-const Type    = require('union-type')
 
-const component = require('../lib/component')
+const { createAll, handle } = require('../lib/actions')
+const redux = require('../lib/redux')
 const { targetVal } = require('../lib/util')
 
-const init = K({
-  content: ''
+const initial = { content: '' }
+
+const Action = createAll([
+  'Change',
+  'Reset'
+])
+
+const reducer = handle(initial, {
+  Change: flip(assoc('content')),
+  Reset:  assoc('content', '')
 })
 
-const Msg = Type({
-  Change: [ String ]
-})
-
-const update = Msg.caseOn({
-  Change: assoc('content')
-})
-
-const view = (model, update) =>
+const view = (model, dispatch) =>
   m('div', { className: css.root }, [
     m('style', css.toString()),
+
+    m('button', {
+      className: css.btn,
+      onclick: compose(dispatch, K(Action.Reset()))
+    }, 'Reset'),
 
     m('input', {
       className: css.input,
       autofocus: true,
-      oninput: compose(update, Msg.Change, targetVal),
-      placeholder: 'Text to reverse'
+      oninput: compose(dispatch, Action.Change, targetVal),
+      placeholder: 'Text to reverse',
+      value: model.content
     }),
 
     m('span', { className: css.arrow }, '<=>'),
@@ -42,13 +49,28 @@ const view = (model, update) =>
     })
   ])
 
-module.exports = component({ init, update, view })
+module.exports = redux({ reducer, view })
 
 const spacing = '2rem'
 
 const css = j2c.sheet({
   '.arrow': {
     marginRight: spacing,
+  },
+
+  '.btn': {
+    background: '#fff',
+    border: '1px solid #ccc',
+    borderRadius: '0.2rem',
+    cursor: 'pointer',
+    marginRight: '1rem',
+    minWidth: '3rem',
+    padding: '0.8rem 1.2rem',
+    outline: 'none',
+
+    '&:active': {
+      background: '#eee'
+    }
   },
 
   '.input': {
