bc75adf537a51add34879e1330752fc86ede3163
Scott McCormack <scott.mccormack@articulate.com>
2016-08-20 23:56:54 -0400

    Cleanup example 4

--- client/views/random.js
+++ client/views/random.js
@@ -1,52 +1,17 @@
 const compose = require('ramda/src/compose')
-const j2c     = require('j2c')
-const K       = require('ramda/src/always')
+const K = require('ramda/src/always')
 
-const { h }  = require('../lib/redux')
-const IO     = require('../lib/io')
-const random = require('../ducks/random')
+const { h } = require('../lib/redux')
+const IO    = require('../lib/io')
+const { setFace } = require('../ducks/random')
 
 const roll = IO(Math.random).map(x => Math.ceil(x * 6))
 
-module.exports = ({ random: state }) =>
-  h('div', { attrs: { class: css.root } }, [
-    h('style', css.toString()),
+module.exports = ({ random: { face } }) =>
+  h('div.random', [
+    h('h1.face', face),
 
-    h('h1', { attrs: { class: css.face } }, state.face),
-
-    h('button', {
-      attrs: { class: css.btn },
-      on: { click: compose(random.setFace, IO.runIO, K(roll)) }
+    h('button.btn.roll', {
+      on: { click: compose(setFace, IO.runIO, K(roll)) }
     }, 'Roll')
   ])
-
-const spacing = '1rem'
-
-const css = j2c.sheet({
-  '.btn': {
-    background: '#fff',
-    border: '0.1rem solid #ccc',
-    borderRadius: '0.2rem',
-    cursor: 'pointer',
-    marginBottom: spacing,
-    outline: 'none',
-    padding: '0.8rem 1.2rem',
-
-    '&:active': {
-      background: '#eee'
-    }
-  },
-
-  '.face': {
-    margin: '0',
-    padding: spacing,
-    textAlign: 'center'
-  },
-
-  '.root': {
-    display: 'flex',
-    flexDirection: 'column',
-    margin: '2rem',
-    width: '5rem'
-  }
-})
