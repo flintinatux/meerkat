f9c9f4e8b285f7b8d1d92654bbba111a4a9120e8
Scott McCormack <scott.mccormack@articulate.com>
2016-08-20 23:30:42 -0400

    Cleanup example 2

--- client/views/inputs.js
+++ client/views/inputs.js
@@ -1,80 +1,32 @@
 const compose = require('ramda/src/compose')
-const j2c     = require('j2c')
 const reverse = require('ramda/src/reverse')
 
+const { change, reset } = require('../ducks/inputs')
 const { h } = require('../lib/redux')
-const inputs = require('../ducks/inputs')
 const { targetVal } = require('../lib/util')
 
-module.exports = ({ inputs: state }) =>
-  h('div', { attrs: { class: css.root } }, [
-    h('style', css.toString()),
-
-    h('button', {
-      attrs: { class: css.btn },
-      on: { click: inputs.reset }
+module.exports = ({ inputs: { content } }) =>
+  h('div.inputs', [
+    h('button.btn', {
+      on: { click: reset }
     }, 'Reset'),
 
-    h('input', {
+    h('input.input.content', {
       attrs: {
-        class: css.input,
         autofocus: true,
         placeholder: 'Text to reverse'
       },
-      on: { input: compose(inputs.change, targetVal) },
-      props: { value: state.content }
+      on: { input: compose(change, targetVal) },
+      props: { value: content }
     }),
 
-    h('span', { attrs: { class: css.arrow } }, '<=>'),
+    h('span', '<=>'),
 
-    h('input', {
+    h('input.input.reverse', {
       attrs: {
-        class: [css.input, css.reverse].join(' '),
         disabled: true,
         placeholder: 'Reversed result'
       },
-      props: { value: reverse(state.content) }
+      props: { value: reverse(content) }
     })
   ])
-
-const spacing = '2rem'
-
-const css = j2c.sheet({
-  '.arrow': {
-    marginRight: spacing,
-  },
-
-  '.btn': {
-    background: '#fff',
-    border: '1px solid #ccc',
-    borderRadius: '0.2rem',
-    cursor: 'pointer',
-    marginRight: '1rem',
-    minWidth: '3rem',
-    padding: '0.8rem 1.2rem',
-    outline: 'none',
-
-    '&:active': {
-      background: '#eee'
-    }
-  },
-
-  '.input': {
-    border: '1px solid #ccc',
-    borderRadius: '0.2rem',
-    marginRight: spacing,
-    outline: 'none',
-    padding: '0.8rem 1.2rem',
-    width: '20rem',
-
-    '&.reverse': {
-      textAlign: 'right'
-    }
-  },
-
-  '.root': {
-    alignItems: 'center',
-    display: 'flex',
-    padding: spacing
-  }
-})
