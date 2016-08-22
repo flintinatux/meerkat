a6cb71fa096ad30df2dd399f47937d59c8635ace
Scott McCormack <scott.mccormack@articulate.com>
2016-08-21 23:50:21 -0400

    Simplify layout and add active class

--- client/views/layout.js
+++ client/views/layout.js
@@ -3,15 +3,20 @@ const { h } = require('../lib/redux')
 module.exports = content => state =>
   h('div.layout', [
     h('nav.nav', [
-      h('a.page', { attrs: { href: '#!/counter' } }, '01 - Counter'),
-      h('a.page', { attrs: { href: '#!/inputs'  } }, '02 - Inputs' ),
-      h('a.page', { attrs: { href: '#!/forms'   } }, '03 - Forms'  ),
-      h('a.page', { attrs: { href: '#!/random'  } }, '04 - Random' ),
-      h('a.page', { attrs: { href: '#!/http'    } }, '05 - HTTP'   ),
-      h('a.page', { attrs: { href: '#!/time'    } }, '06 - Time'   ),
-      h('a.page', { attrs: { href: '#!/sockets' } }, '07 - Sockets')
+      link('#!/counter', '01 - Counter'),
+      link('#!/inputs',  '02 - Inputs' ),
+      link('#!/forms',   '03 - Forms'  ),
+      link('#!/random',  '04 - Random' ),
+      link('#!/http',    '05 - HTTP'   ),
+      link('#!/time',    '06 - Time'   ),
+      link('#!/sockets', '07 - Sockets')
     ]),
 
     h('section.content', [ content(state) ])
   ])
 
+const link = (href, children='') =>
+  h('a.link', {
+    attrs: { href },
+    class: { active: location.hash === href }
+  }, children)
