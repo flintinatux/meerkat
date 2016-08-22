4d9a43e883a812ef1184fa2180c8206868e21c4c
Scott McCormack <scott.mccormack@articulate.com>
2016-08-21 23:00:42 -0400

    Cleanup sockets example

--- client/views/sockets.js
+++ client/views/sockets.js
@@ -1,5 +1,4 @@
 const compose = require('ramda/src/compose')
-const j2c     = require('j2c')
 const K       = require('ramda/src/always')
 const prop    = require('ramda/src/prop')
 const trim    = require('ramda/src/trim')
@@ -8,80 +7,36 @@ const { h } = require('../lib/redux')
 const idgen = require('../lib/idgen')
 const { listToArray } = require('../lib/util')
 const { preventDefault, targetVal }  = require('../lib/util')
-const sockets = require('../ducks/sockets')
+const { body, insertMessage, reset } = require('../ducks/sockets')
 
 const socket = new WebSocket('ws://echo.websocket.org')
 
 const connect = dispatch =>
-  socket.onmessage = compose(dispatch, sockets.insertMessage, JSON.parse, prop('data'))
+  socket.onmessage = compose(dispatch, insertMessage, JSON.parse, prop('data'))
 
 const disconnect = _ => socket.onmessage = null
 
 const send = body => socket.send(JSON.stringify({ id: idgen(), body }))
 
-module.exports = ({ sockets: state }) =>
-  h('div', { attrs: { class: css.root } }, [
-    h('style', css.toString()),
-
-    h('div', {
-      attrs: { class: css.messages },
+module.exports = ({ sockets }) =>
+  h('div.sockets', [
+    h('div.messages', {
       redux: { create: connect, destroy: disconnect }
-    }, listToArray(state.messages).map(msg =>
-      h('div', { attrs: { class: css.message } }, msg.body)
+    }, listToArray(sockets.messages).map(msg =>
+      h('div.message', msg.body)
     )),
 
-    h('form', {
-      attrs: { class: css.form },
+    h('form.form', {
       on: {
-        submit: compose(sockets.reset, send, K(state.body), preventDefault)
+        submit: compose(reset, send, K(sockets.body), preventDefault)
       }
     }, [
-      h('input', {
-        attrs: { class: css.body },
-        on: { input: compose(sockets.body, trim, targetVal) },
-        props: { value: state.body }
+      h('input.input.body', {
+        on: { input: compose(body, trim, targetVal) },
+        props: { value: sockets.body }
       }),
-      h('button', {
-        attrs: {
-          class: css.btn,
-          disabled: !state.body
-        }
+      h('button.btn.send', {
+        attrs: { disabled: !sockets.body }
       }, 'Send')
     ])
   ])
-
-const spacing = '2rem'
-
-const css = j2c.sheet({
-  '.body': {
-    border: '0.1rem solid #ccc',
-    borderRadius: '0.2rem 0 0 0.2rem',
-    borderRight: 'none',
-    flexGrow: 2,
-    outline: 'none',
-    padding: '0.8rem 1.2rem'
-  },
-
-  '.btn': {
-    background: '#fff',
-    border: '0.1rem solid #ccc',
-    borderRadius: '0 0.2rem 0.2rem 0',
-    cursor: 'pointer',
-    outline: 'none',
-    flexShrink: 0,
-    padding: '0.8rem 1.2rem'
-  },
-
-  '.form': {
-    display: 'flex'
-  },
-
-  '.message': {
-    paddingBottom: '1rem'
-  },
-
-  '.root': {
-    maxWidth: '50rem',
-    padding: spacing
-  }
-})
