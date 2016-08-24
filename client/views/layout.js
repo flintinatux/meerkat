102ad74e191bd2ba67ac7603fe5920609c46b37b
Scott McCormack <scott.mccormack@articulate.com>
2016-08-24 02:39:47 -0400

    Try href helper

--- client/views/layout.js
+++ client/views/layout.js
@@ -1,15 +1,15 @@
-const { h } = require('puddles')
+const { h, route: { href } } = require('puddles')
 
 const layout = content => state =>
   h('div.layout', [
     h('nav.nav', [
-      link('#!/counter', '01 - Counter'),
-      link('#!/inputs',  '02 - Inputs' ),
-      link('#!/forms',   '03 - Forms'  ),
-      link('#!/random',  '04 - Random' ),
-      link('#!/http',    '05 - HTTP'   ),
-      link('#!/time',    '06 - Time'   ),
-      link('#!/sockets', '07 - Sockets')
+      link(href('/counter'), '01 - Counter'),
+      link(href('/inputs'),  '02 - Inputs' ),
+      link(href('/forms'),   '03 - Forms'  ),
+      link(href('/random'),  '04 - Random' ),
+      link(href('/http'),    '05 - HTTP'   ),
+      link(href('/time'),    '06 - Time'   ),
+      link(href('/sockets'), '07 - Sockets')
     ]),
 
     h('section.content', [ content(state) ])
